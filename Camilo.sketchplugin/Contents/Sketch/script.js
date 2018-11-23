ec9176087c122823fe93fa672058ae9513e61071
egarcia <egarcia@tuenti.com>
2018-11-23 14:08:43 -0200

    transforming global variables into local variables to avoid global behaviour

--- Camilo.sketchplugin/Contents/Sketch/script.js
+++ Camilo.sketchplugin/Contents/Sketch/script.js
@@ -1,3 +1,8 @@
+var PLUGIN_NAME = 'Camilo';
+var PLUGIN_LOGO = 'icon.png';
+
+var SYMBOL_MASTER = 'MSSymbolMaster';
+var SYMBOL_INSTANCE = 'MSSymbolInstance';
 
 var createLookup = function (styles) {
 	var lookup = {};
@@ -32,8 +37,8 @@ var syncLibraryStyles = function (libraryStyles, documentStyles, lookup) {
 function googleAnalytics(context, category, action, label, value) {
 	var trackingID = "UA-128191866-1";
 	var	uuidKey = "google.analytics.uuid";
-	var	uuid = NSUserDefaults.standardUserDefaults().objectForKey(uuidKey);
 	var url = "https://www.google-analytics.com/collect?v=1";
+	var	uuid = NSUserDefaults.standardUserDefaults().objectForKey(uuidKey);
 
 	if (!uuid) {
 		uuid = NSUUID.UUID().UUIDString();
@@ -72,7 +77,7 @@ function googleAnalytics(context, category, action, label, value) {
 
 function getInstances(context) {
 	var instanceArray = NSArray.array();
-	var	predicate = NSPredicate.predicateWithFormat("className == %@", "MSSymbolInstance");
+	var	predicate = NSPredicate.predicateWithFormat("className == %@", SYMBOL_INSTANCE);
 	var	pageLoop = context.document.pages().objectEnumerator();
 	var	page;
 
@@ -90,7 +95,24 @@ function getSymbolArray(librarySymbols) {
 		NSMutableArray.arrayWithArray(["No Symbols"]);
 }
 
-function getLibrary(context, chosenLibrary) {
+function getLibrarySymbols(library) {
+	var librarySymbolSort = NSSortDescriptor.sortDescriptorWithKey_ascending("name", 1);
+	var	librarySymbols;
+
+	if (library == 0) {
+		librarySymbols = MSDocument.currentDocument().documentData().localSymbols();
+	} else {
+		var libraryPath = library.locationOnDisk().path();
+		var	libraryFile = openFile(libraryPath);
+
+		librarySymbols = (libraryFile) ? libraryFile.documentData().allSymbols() : nil;
+		libraryFile.close();
+	}
+
+	return librarySymbols.sortedArrayUsingDescriptors([librarySymbolSort]);
+}
+
+function getLibrary(context, chosenLibrary, libraries, libraryNames) {
 	var lastLibrary = context.command.valueForKey_onLayer("lastLibrary", context.document.documentData());
 	var	library = 0;
 
@@ -119,25 +141,9 @@ function getLibrary(context, chosenLibrary) {
 
 	return {
 		selectedLibrary : selectedLibraryIndex,
+		librarySymbols,
+		symbolArray
 	}
-}
-
-function getLibrarySymbols(library) {
-	var librarySymbolSort = NSSortDescriptor.sortDescriptorWithKey_ascending("name", 1);
-	var	librarySymbols;
-
-	if (library == 0) {
-		librarySymbols = MSDocument.currentDocument().documentData().localSymbols();
-	} else {
-		var libraryPath = library.locationOnDisk().path();
-		var	libraryFile = openFile(libraryPath);
-
-		librarySymbols = (libraryFile) ? libraryFile.documentData().allSymbols() : nil;
-
-		libraryFile.close();
-	}
-
-	return librarySymbols.sortedArrayUsingDescriptors([librarySymbolSort]);
 }
 
 function importForeignSymbol(symbol,library) {
@@ -146,10 +152,10 @@ function importForeignSymbol(symbol,library) {
 	var	foreignSymbol;
 
 	if (MSApplicationMetadata.metadata().appVersion >= 50) {
-		var objectReference = MSShareableObjectReference.referenceForShareableObject_inLibrary(symbol,library);
-		foreignSymbol = libraryController.importShareableObjectReference_intoDocument(objectReference,intoDocument);
+		var objectReference = MSShareableObjectReference.referenceForShareableObject_inLibrary(symbol, library);
+		foreignSymbol = libraryController.importShareableObjectReference_intoDocument(objectReference, intoDocument);
 	} else {
-		foreignSymbol = libraryController.importForeignSymbol_fromLibrary_intoDocument_(symbol,library,intoDocument);
+		foreignSymbol = libraryController.importForeignSymbol_fromLibrary_intoDocument_(symbol, library, intoDocument);
 	}
 
 	return foreignSymbol;
@@ -157,7 +163,7 @@ function importForeignSymbol(symbol,library) {
 
 function openFile(path) {
 	var file = MSDocument.new();
-	return (file.readFromURL_ofType_error(path, 'com.bohemiancoding.sketch.drawing',nil)) ? file : nil;
+	return (file.readFromURL_ofType_error(path, 'com.bohemiancoding.sketch.drawing', nil)) ? file : nil;
 }
 
 function getOptionSelected() {
@@ -175,14 +181,11 @@ function getOptionSelected() {
 }
 
 function proceed(selection, symbolMaster) {
-	if (selection.class() == "MSSymbolMaster") {
-		var instances = selection.allInstances();
-
-		for (var i = 0; i < instances.length; i++) {
-			instances[i].changeInstanceToSymbol(symbolMaster);
-			instances[i].setName(symbolMaster.name());
-		}
-		
+	if (selection.class() == SYMBOL_MASTER) {
+		selection.allInstances().forEach(function (instance) {
+			instance.changeInstanceToSymbol(symbolMaster);
+			instance.setName(symbolMaster.name());
+		});
 		selection.removeFromParent();
 		selection.setName(symbolMaster.name());
 	}
@@ -202,14 +205,26 @@ function getSymbolMaster(symbol, library) {
 
 var replaceSymbols = function(context, chosenLibrary) {
 	var docSymbols = context.document.documentData().allSymbols();
-	var	predicate = NSPredicate.predicateWithFormat("className == %@ || className == %@", "MSSymbolMaster", "MSSymbolInstance");
+	var	predicate = NSPredicate.predicateWithFormat("className == %@ || className == %@", SYMBOL_MASTER, SYMBOL_INSTANCE);
 	var	selection = docSymbols.filteredArrayUsingPredicate(predicate);
 
 	if (selection.length) {
-		var librarySettings = getLibrary(context, chosenLibrary);
+		var librarySort = NSSortDescriptor.sortDescriptorWithKey_ascending("name", 1);
+		var	libraries = AppController.sharedInstance().librariesController().libraries().sortedArrayUsingDescriptors([librarySort]);
+		var	libraryLoop = libraries.objectEnumerator();
+		var	libraryNames = ["Current Document"];
+		var	library;
+
+		while (library = libraryLoop.nextObject()) {
+			libraryNames.push(String(library.name()));
+		}
+
+		var librarySettings = getLibrary(context, chosenLibrary, libraries, libraryNames);
 
 		if (librarySettings) {
 			var selectedLibrary = (librarySettings.selectedLibrary != 0) ? libraries[librarySettings.selectedLibrary - 1] : 0;
+			var librarySymbols = librarySettings.librarySymbols;
+			var symbolArray = librarySettings.symbolArray;
 			var	selectedMaster = librarySettings.selectedMaster;
 			var	selectionLoop = selection.objectEnumerator();
 			var	selection;
@@ -222,7 +237,7 @@ var replaceSymbols = function(context, chosenLibrary) {
 					instanceMap[selection.symbolID().toString()] = symbolMaster.symbolID().toString();
 					proceed(selection, symbolMaster);
 				} else {
-					var symbolName = (selection.class() == "MSSymbolMaster") ? 
+					var symbolName = (selection.class() == SYMBOL_MASTER) ? 
 						selection.name() : 
 						selection.symbolMaster().name();
 
@@ -235,18 +250,18 @@ var replaceSymbols = function(context, chosenLibrary) {
 				}
 			}
 
-			var allInstances = getInstances(context);
-			for (var i = 0; i < allInstances.length; i++) {
+			getInstances(context).forEach(function (instance) {
 				if (!MSLayerPaster.updateOverridesOnInstance_withIDMap_) {
-						allInstances[i].updateOverridesWithObjectIDMap(instanceMap);
+					instance.updateOverridesWithObjectIDMap(instanceMap);
 				} else {
-						MSLayerPaster.updateOverridesOnInstance_withIDMap_(allInstances[i],instanceMap);
+					MSLayerPaster.updateOverridesOnInstance_withIDMap_(instance, instanceMap);
 				}
-			}
+			});
 			context.document.reloadInspector();			
 		}
 	} else {
 		displayDialog("Please select at least one symbol master or instance.");
+		googleAnalytics(context, 'legacy displayDialog shown', 'legacy', 'legacy');
 	}
 }
 
@@ -260,10 +275,10 @@ var replaceStyles = function (context) {
   	};
 	var alert = COSAlertWindow.new();
 	  
-  	alert.setMessageText('Camilo');
+  	alert.setMessageText(PLUGIN_NAME);
   	alert.setInformativeText("Select a theme library to replace magically document's layer styles and symbols 🎉")
   	// add new logo
-  	alert.setIcon(NSImage.alloc().initByReferencingFile(context.plugin.urlForResourceNamed("icon.png").path()));
+  	alert.setIcon(NSImage.alloc().initByReferencingFile(context.plugin.urlForResourceNamed(PLUGIN_LOGO).path()));
   	alert.addAccessoryView(getOptionSelected());
   	alert.addButtonWithTitle('Sync');
 	alert.addButtonWithTitle('Cancel');
@@ -283,16 +298,3 @@ var replaceStyles = function (context) {
 		});
 	}
 };
-
-var librarySort = NSSortDescriptor.sortDescriptorWithKey_ascending("name", 1);
-var	libraries = AppController.sharedInstance().librariesController().libraries().sortedArrayUsingDescriptors([librarySort]);
-var	libraryLoop = libraries.objectEnumerator();
-var	library;
-var	libraryNames = ["Current Document"];
-var	librarySymbols;
-var	librarySelects = [];
-var	symbolArray;
-
-while (library = libraryLoop.nextObject()) {
-	libraryNames.push(String(library.name()));
-}
