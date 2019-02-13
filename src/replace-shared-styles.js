837f738d3e1ab46661b8aeac8f7d67dfe411ec01
Pausansol <pausansol@gmail.com>
2019-02-13 23:30:39 +0100

    relive the comments

--- src/replace-shared-styles.js
+++ src/replace-shared-styles.js
@@ -4,7 +4,10 @@ export default function(libraryStyles, lookup, library) {
     const currentSharedStyle = lookup[librarySharedStyle.name]
     if (currentSharedStyle) {
       const imported = currentSharedStyle.import()
-      // if the shared style is coming from a library:
+      // if the shared style is coming from a library, then we just want to:
+      // - import the matching style
+      // - update all the instances to point to the imported style
+      // - map the current style to the imported one
       if (librarySharedStyle.getLibrary()) {
         map[librarySharedStyle.id] = imported.id
         librarySharedStyle.getAllInstancesLayers().forEach(l => {
@@ -13,7 +16,9 @@ export default function(libraryStyles, lookup, library) {
           l.style.syncWithSharedStyle(imported)
         })
       } else {
-        // if the shared style is local:
+        // if the shared style is local, then we just want to:
+        // - import the matching style to update the local one
+        // - update all the instances to point to the imported style
         librarySharedStyle.style = imported.style
         librarySharedStyle
           .getAllInstances()
