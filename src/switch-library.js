c6421beb24384f71089911f33c36f3c8d881686c
Pablo Sanchez Soler <pausansol@gmail.com>
2020-11-06 12:49:44 +0100

    Update switch-library.js

--- src/switch-library.js
+++ src/switch-library.js
@@ -15,17 +15,21 @@ export default function(document, library) {
     lookup.layer,
     library
   )
+  // replace the textStyles
   const textStylesMap = replaceSharedStyles(
     document.getSharedTextStyles(),
     lookup.text,
     library
   )
-
+  // replace the overrides
   replaceOverrides(docSymbolInstances, {
     symbolsMap,
     layerStylesMap,
     textStylesMap,
   })
+
+  // replace the swatches
+  
 
   // reload the inspector to make sure we show the latest changes
   document.sketchObject.reloadInspector()
